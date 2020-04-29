761d40ebbe02415fe7bfc52edd13396ee4ff99dd
ist <pratik.mahajan@innoeye.com>
2020-04-30 02:03:52 +0530

    Initial Maven Project Structure

---
+++ parent-project/webapp/src/main/java/com/inn/myapp/Starter.java
@@ -0,0 +1,28 @@
+package com.inn.myapp;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
+
+@SpringBootApplication
+@ComponentScan
+public class Starter extends SpringBootServletInitializer implements WebApplicationInitializer{
+	
+	   @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return configureApplication(builder);
+	    }
+
+	    public static void main(String[] args) {
+	        configureApplication(new SpringApplicationBuilder()).run(args);
+	    }
+
+	    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+	        return builder.sources(Starter.class).bannerMode(Banner.Mode.OFF);
+	    }
+
+	
+}
