7b62371867a9321a1222969554894779608c6997
ist <pratik.mahajan@innoeye.com>
2020-05-01 22:10:50 +0530

    Changes

--- parent-project/product-controllers/src/main/java/com/inn/myapp/TestController.java
+++ parent-project/product-controllers/src/main/java/com/inn/myapp/TestController.java
@@ -3,6 +3,7 @@ package com.inn.myapp;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 	
-	
+	@GetMapping(path="get")
 	public List<String> get(){
 
 		return Arrays.asList("Pratik" , "Niyank" );
