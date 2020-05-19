da83599109bc7e7c6333176610b98b8d5c7f4b57
ist <pratik.mahajan@innoeye.com>
2020-05-19 22:05:23 +0530

    new rest layer added

--- parent-project/product-abstract-base/src/main/java/com/inn/myapp/rest/UserController.java
+++ parent-project/product-abstract-base/src/main/java/com/inn/myapp/rest/UserController.java
@@ -3,6 +3,7 @@ package com.inn.myapp.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@GetMapping(path="/getAllActiveUsers")
 	public List<User> getAllActiveUsers(){
 		return userService.getAllActiveUser();
 	}
