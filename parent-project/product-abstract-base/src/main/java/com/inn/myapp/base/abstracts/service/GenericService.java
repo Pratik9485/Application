7fa4c2e9e48113cb331169e633078eb1ff9afb82
pratik.mahajan <pratik.mahajan@innoeye.com>
2020-08-22 13:19:44 +0530

    Change

--- parent-project/product-abstract-base/src/main/java/com/inn/myapp/base/abstracts/service/GenericService.java
+++ parent-project/product-abstract-base/src/main/java/com/inn/myapp/base/abstracts/service/GenericService.java
@@ -2,7 +2,7 @@ package com.inn.myapp.base.abstracts.service;
 
 import com.inn.myapp.base.abstracts.entity.AbstractEntity;
 
- abstract class  GenericService<T extends AbstractEntity<N> , N> implements AbstractService<T, N> {
+abstract class  GenericService<T extends AbstractEntity<N> , N> implements AbstractService<T, N> {
 	
     public final T create(T entity) {
             return null;
